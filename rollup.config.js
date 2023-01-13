0a0e1ce9b14e5df78524970334d9054389670c30
Esteban Borai <estebanborai@gmail.com>
2023-01-13 18:20:35 -0400

    fix: produce ES modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
   output: [
-    { dir: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
     {
-      dir: pkg.main,
+      file: pkg.main,
       format: 'umd',
       name: '@whizzes/svelte-forms',
       sourcemap: true,
